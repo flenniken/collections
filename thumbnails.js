746cb97452799e1bf34e25f9dc0fdc426ef64b31
Steve Flenniken <steve.flenniken@gmail.com>
2023-06-23 11:35:30 -0700

    Show the width and height of the page.

---
+++ thumbnails.js
@@ -0,0 +1,20 @@
+// thumbnails.js
+
+window.addEventListener("load", showPageSize)
+window.addEventListener("resize", showPageSize)
+
+function showPageSize() {
+  // Show the width and height of the page.
+
+  var width = window.innerWidth || document.documentElement.clientWidth ||
+    document.body.clientWidth;
+
+  var height = window.innerHeight || document.documentElement.clientHeight ||
+      document.body.clientHeight;
+
+  // var availWidth = window.screen.availWidth
+  // var availHeight = window.screen.availHeight
+
+  document.getElementById("width").innerHTML = width.toString()
+  document.getElementById("height").innerHTML = height.toString()
+}
