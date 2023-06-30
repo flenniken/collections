be2167b785ad9673f36f3b551336c9f8d9543fe1
Steve Flenniken <steve.flenniken@gmail.com>
2023-06-30 09:04:36 -0700

    Add swipe to the image page.

--- js/image.js
+++ js/image.js
@@ -1,5 +1,56 @@
 // image.js
 
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}
+
+function handleTouchStart(evt) {
+  const firstTouch = getTouches(evt)[0];
+  xDown = firstTouch.clientX;
+  yDown = firstTouch.clientY;
+};
+
+function handleTouchMove(evt) {
+  if ( ! xDown || ! yDown ) {
+    return;
+  }
+
+  var xUp = evt.touches[0].clientX;
+  var yUp = evt.touches[0].clientY;
+
+  var xDiff = xDown - xUp;
+  var yDiff = yDown - yUp;
+
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    if ( xDiff > 0 ) {
+      console.log(`pull left: xDiff = ${xDiff}`);
+      /* pull right */
+    } else {
+      console.log(`pull right: xDiff = ${xDiff}`);
+      /* pull left */
+    }
+  } else {
+    console.log(`yDiff: ${yDiff}`);
+    if ( yDiff > 0 ) {
+      console.log(`pull up: xDiff = ${xDiff}`);
+      /* pull up */
+    } else {
+      /* pull down */
+      console.log(`pull down: xDiff = ${xDiff}`);
+      window.location.href = "thumbnails-1.html"
+    }
+  }
+  xDown = null;
+  yDown = null;
+};
+
 window.addEventListener("load", sizeImage)
 window.addEventListener("resize", sizeImage)
 
