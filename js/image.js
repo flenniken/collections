97d4d15a9f4d5005d6e3769a8f9d31e40a1497e8
Steve Flenniken <steve.flenniken@gmail.com>
2023-07-01 14:03:56 -0700

    Switch img.src when switching images.

--- js/image.js
+++ js/image.js
@@ -27,71 +27,23 @@ function nextImage() {
     imageIx = 0
   console.log(`next image. imageIx = ${imageIx}`);
   setImage(imageIx)
-  sizeImage()
 }
 
 function setImage(ix) {
   // Set the current image.
   let image = cJson.images[ix]
-  let img = `<img id="image" width="${image.width}" height="${image.height}" src="${image.url}" alt="${image.title}">`
-  console.log(`${img}`);
-  document.getElementById("imageDiv").innerHTML = img
+  var img = document.getElementById("image")
+  img.src = image.url
+  img.alt = image.alt
+  img.style.width = `${image.width}px`;
+  img.style.height = `${image.height}px`;
+  sizeImage()
 }
 
 // Read the collection's json file.
 fetch('../pages/collection-1.json')
     .then((response) => response.json())
     .then((json) => cJson = json);
-
-// Handle swiping.
-document.addEventListener('touchstart', handleTouchStart, false);
-document.addEventListener('touchmove', handleTouchMove, false);
-
-var xDown = null;
-var yDown = null;
-
-function getTouches(evt) {
-  return evt.touches ||             // browser API
-         evt.originalEvent.touches; // jQuery
-}
-
-function handleTouchStart(evt) {
-  const firstTouch = getTouches(evt)[0];
-  xDown = firstTouch.clientX;
-  yDown = firstTouch.clientY;
-};
-
-function handleTouchMove(evt) {
-  if ( ! xDown || ! yDown ) {
-    return;
-  }
-
-  var xUp = evt.touches[0].clientX;
-  var yUp = evt.touches[0].clientY;
-
-  var xDiff = xDown - xUp;
-  var yDiff = yDown - yUp;
-
-  if (Math.abs(xDiff) > Math.abs(yDiff)) {
-    if ( xDiff > 0 ) {
-      console.log(`swipe right: xDiff = ${xDiff}`);
-      nextImage()
-    } else {
-      console.log(`swipe left: xDiff = ${xDiff}`);
-      previousImage()
-    }
-  } else {
-    console.log(`yDiff: ${yDiff}`);
-    if ( yDiff > 0 ) {
-      console.log(`swipe up: xDiff = ${xDiff}`);
-    } else {
-      console.log(`swipe down: xDiff = ${xDiff}`);
-      window.location.href = "thumbnails-1.html"
-    }
-  }
-  xDown = null;
-  yDown = null;
-};
 
 window.addEventListener("load", sizeImage)
 window.addEventListener("resize", sizeImage)
@@ -113,4 +65,58 @@ function setImageSizeByWidth(img, newWidth) {
   const newHeight = (newWidth * oldHeight)/oldWidth;
   img.style.width = `${newWidth}px`;
   img.style.height = `${newHeight}px`;
+}
+
+document.addEventListener('touchstart', handleTouchStart, false)
+document.addEventListener('touchmove', handleTouchMove, false)
+document.addEventListener('touchend', handleTouchEnd, false)
+document.addEventListener('touchcancel', handleTouchEnd, false)
+
+var xDown = null
+var yDown = null
+var xPt = null
+var yPt = null
+
+function handleTouchStart(evt) {
+  const firstTouch = evt.touches[0]
+  xDown = firstTouch.clientX
+  yDown = firstTouch.clientY
+}
+
+function handleTouchMove(evt) {
+  if (!xDown || !yDown)
+    return
+  xPt = evt.touches[0].clientX
+  yPt = evt.touches[0].clientY
+  console.log(`${xPt},${yPt}`)
+}
+
+function handleTouchEnd(evt) {
+  if (!xDown || !yDown)
+    return
+  if (!xPt || !yPt)
+    return
+  const xDiff = xDown - xPt
+  const yDiff = yDown - yPt
+
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    if (xDiff > 0) {
+      console.log(`swipe right: xDiff = ${xDiff}`)
+      nextImage()
+    } else {
+      console.log(`swipe left: xDiff = ${xDiff}`)
+      previousImage()
+    }
+  } else {
+    if (yDiff > 0) {
+      console.log(`swipe up: yDiff = ${yDiff}`)
+    } else {
+      console.log(`swipe down: yDiff = ${yDiff}`)
+      window.location.href = "thumbnails-1.html"
+    }
+  }
+  xDown = null
+  yDown = null
+  xPt = null
+  yPt = null
 }
