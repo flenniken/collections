6422db77b19d2cc8f6dc5d3aa5de50cfa4c598c2
Steve Flenniken <steve.flenniken@gmail.com>
2023-11-05 14:07:25 -0800

    Add link to thumbnails page.

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -2,7 +2,6 @@
 "use strict";
 
 window.addEventListener("load", showPageSize)
-window.addEventListener("resize", showPageSize)
 
 function showPageSize() {
   // Show the width and height of the page.
@@ -18,58 +17,4 @@ function showPageSize() {
 
   document.getElementById("width").innerHTML = width.toString()
   document.getElementById("height").innerHTML = height.toString()
-}
-
-// Handle swiping down to go back to the collections page.
-
-document.addEventListener('touchstart', handleTouchStart, false)
-document.addEventListener('touchmove', handleTouchMove, false)
-document.addEventListener('touchend', handleTouchEnd, false)
-document.addEventListener('touchcancel', handleTouchEnd, false)
-
-var xDown = null
-var yDown = null
-var xPt = null
-var yPt = null
-
-function handleTouchStart(evt) {
-  const firstTouch = evt.touches[0]
-  xDown = firstTouch.clientX
-  yDown = firstTouch.clientY
-}
-
-function handleTouchMove(evt) {
-  if (!xDown || !yDown)
-    return
-  xPt = evt.touches[0].clientX
-  yPt = evt.touches[0].clientY
-  console.log(`${xPt},${yPt}`)
-}
-
-function handleTouchEnd(evt) {
-  if (!xDown || !yDown)
-    return
-  if (!xPt || !yPt)
-    return
-  const xDiff = xDown - xPt
-  const yDiff = yDown - yPt
-
-  if (Math.abs(xDiff) > Math.abs(yDiff)) {
-    if (xDiff > 0) {
-      console.log(`swipe right: xDiff = ${xDiff}`)
-    } else {
-      console.log(`swipe left: xDiff = ${xDiff}`)
-    }
-  } else {
-    if (yDiff > 0) {
-      console.log(`swipe up: yDiff = ${yDiff}`)
-    } else {
-      console.log(`swipe down: yDiff = ${yDiff}`)
-      window.location.href = "../index.html"
-    }
-  }
-  xDown = null
-  yDown = null
-  xPt = null
-  yPt = null
 }
