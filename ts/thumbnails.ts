3d3bf3e22b53357625cc9b701b8ee83c696b4bc1
Steve Flenniken <steve.flenniken@gmail.com>
2024-01-12 15:44:32 -0800

    Style the thumbnails title and description list the image page.

--- ts/thumbnails.ts
+++ ts/thumbnails.ts
@@ -1,7 +1,5 @@
 // Code for the thumbnails page.
 
-window.addEventListener("load", showPageSize)
-
 function get(id: string) {
   // Get the dom element with the given id. Generate and exception
   // when not found.
@@ -9,20 +7,4 @@ function get(id: string) {
   if (!element)
     throw new Error(`Element with "${id}" not found.`)
   return element
-}
-
-function showPageSize() {
-  // Show the width and height of the page.
-
-  var width = window.innerWidth || document.documentElement.clientWidth ||
-    document.body.clientWidth
-
-  var height = window.innerHeight || document.documentElement.clientHeight ||
-      document.body.clientHeight
-
-  // var availWidth = window.screen.availWidth
-  // var availHeight = window.screen.availHeight
-
-  get("width").innerHTML = width.toString()
-  get("height").innerHTML = height.toString()
 }
