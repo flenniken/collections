c2d9b32d4358aba46e0dd197e541e3ab1fa47d98
Steve Flenniken <steve.flenniken@gmail.com>
2024-02-23 16:49:11 -0800

    Add install banner when useful.

--- ts/index.ts
+++ ts/index.ts
@@ -16,5 +16,22 @@ function handleLoad() {
   if (window.matchMedia('(display-mode: standalone)').matches) {
     runningFromIcon = true
     log("Running from the desktop icon.")
+    return
   }
+
+  // On an iPhone, when installing is allowed, we want to show a banner when the
+  // user has not installed it yet.
+
+  log(`navigator.platform: ${navigator.platform}`)
+  if (navigator.platform != 'iPhone') {
+    return
+  }
+
+  if (!("GestureEvent" in window)) {
+    log("not running safari")
+    return
+  }
+
+  log("show install banner")
+  get("install-banner").style.display = "block"
 }
