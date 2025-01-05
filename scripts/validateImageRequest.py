f5cd8658ded47c9720c68fba00f41a2c6395fb79
Steve Flenniken <steve.flenniken@gmail.com>
2025-01-05 13:36:21 -0800

    Update comments.

--- scripts/validateImageRequest.py
+++ scripts/validateImageRequest.py
@@ -12,14 +12,23 @@ import urllib.request
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.serialization import load_pem_public_key
-# Lambda@edge doesn't support the importing libraries (layers) not
-# already part of its system, for example the jwt library.
+# Lambda@edge doesn't support importing external libraries without
+# extra work. You need to deploy your code and its dependencies
+# together in a zip file. The maximum size for a .zip deployment
+# package for Lambda is 250 MB (unzipped).  Lambda@Edge doesn't
+# support layers or containers.
+# See:
+# * https://docs.aws.amazon.com/lambda/latest/dg/python-package.html
+# * https://stackoverflow.com/questions/77986827/cloudfront-lambdaedge-in-nodejs-layer-is-not-supported-for-cloudfront-how-to
 
 # Global variables can be used for caching because AWS Lambda reuses
 # the same container for multiple invocations. When the container is
 # first started and the handler is called, it's known as a "cold
 # start". Subsequent calls to the already running handler are known as
-# "warm starts".
+# "warm starts". You can also cache static assets locally in the /tmp
+# directory. The AWS Lambda SDK for JavaScript reuses TCP connections
+# by default. What about python? The keepAliveMsecs default is 1000
+# ms.  todo: what is the keepAliveMseces for Python?
 
 # Cache the keys (jwks) so we only have to fetch them on a cold start
 # or when the keys change.
@@ -146,17 +155,19 @@ def validateAccessToken(jwks, token):
     global cachedJwks
     cachedJwks = jwks
 
+  # Cognito only uses RS256 and that is the only algorithm this code
+  # supports.
   if header["alg"] != "RS256":
     return "Unsupported header algorithm."
   if key["alg"] != "RS256":
     return "Unsupported key algorithm."
 
-  # Construct the RSA public key
+  # Construct the RSA public key.
   n = int.from_bytes(base64_url_decode(key['n']), 'big')
   e = int.from_bytes(base64_url_decode(key['e']), 'big')
   public_key = rsa.RSAPublicNumbers(e, n).public_key()
 
-  # Verify the signature
+  # Verify the signature.
   try:
     public_key.verify(
       signature,
