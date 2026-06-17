304f7e22f282e6405470d0424d85a3e8e63fe894
Steve Flenniken <steve.flenniken@gmail.com>
2026-06-17 15:50:27 -0700

    Move code to ts file.

--- ts/index.ts
+++ ts/index.ts
@@ -431,3 +431,49 @@ async function logAppCache() {
     })
   })
 }
+
+async function enableNotifications() {
+  if (Notification.permission === 'granted') {
+    console.log("Notifications already enabled.")
+    return
+  }
+  try {
+      const permission = await Notification.requestPermission();
+      console.log(`permission: ${permission}`)
+      if (permission === 'granted') {
+        subscribe();
+      }
+    } catch (error) {
+      console.error('Error requesting notification permission:', error);
+  }
+}
+
+async function subscribe() {
+  try {
+    const registration = await navigator.serviceWorker.ready;
+    const VAPID_PUBLIC_KEY =
+'BPXArEWQz2DQMcdcvK6xMC0q4tsv6igQCQv1FIodqJPQcNzzqY4BzeaF4qX5nHidzmgUXbWGI7eHdELGMjcrda8';
+
+    const subscription = await registration.pushManager.subscribe({
+      userVisibleOnly: true,
+      // Force TypeScript to accept it strictly as an ArrayBuffer
+      applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY).buffer as ArrayBuffer
+    });
+    console.log('Subscription:', subscription);
+  } catch (error) {
+    console.error('Error subscribing to push notifications:', error);
+  }
+}
+
+function urlBase64ToUint8Array(base64String: string): Uint8Array {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/');
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+
+  return outputArray;
+}
